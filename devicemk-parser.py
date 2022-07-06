053e9edfec1b12b0ffdb8c69f6fa59ac06c19f50
Mykola Khyliuk <nkh@ua.fm>
2022-07-06 15:25:20 +0200

    Put get_env_var() and get_build_var() to utils module
    Also added:
    - .gitignore

--- devicemk-parser.py
+++ devicemk-parser.py
@@ -4,6 +4,8 @@ import os
 import re
 import sys
 
+from utils.utils import *
+
 
 class Include:
 
@@ -162,33 +164,6 @@ def parse():
         os.chdir(wd)
 
     return files
-
-
-def get_env_var(name):
-    cached_value = get_env_var.dict.get(name)
-    if cached_value is None:
-        value = os.popen("echo ${name}".format(**locals())).read().rstrip()
-        get_env_var.dict[name] = value
-        # print("name: {name}, value: {value}".format(**locals()))
-        return value
-    return cached_value
-
-
-get_env_var.dict = {}
-
-
-def get_build_var(name):
-    cached_value = get_build_var.dict.get(name)
-    if cached_value is None:
-        value = os.popen(
-            "soong_ui --dumpvar-mode {name}".format(**locals())).read().rstrip()
-        get_build_var.dict[name] = value
-        # print("name: {name}, value: {value}".format(**locals()))
-        return value
-    return cached_value
-
-
-get_build_var.dict = {}
 
 
 def get_device_mk():
---
+++ utils/utils.py
@@ -0,0 +1,27 @@
+import os
+
+def get_env_var(name):
+    cached_value = get_env_var.dict.get(name)
+    if cached_value is None:
+        value = os.popen("echo ${name}".format(**locals())).read().rstrip()
+        get_env_var.dict[name] = value
+        # print("name: {name}, value: {value}".format(**locals()))
+        return value
+    return cached_value
+
+
+get_env_var.dict = {}
+
+
+def get_build_var(name):
+    cached_value = get_build_var.dict.get(name)
+    if cached_value is None:
+        value = os.popen(
+            "soong_ui --dumpvar-mode {name}".format(**locals())).read().rstrip()
+        get_build_var.dict[name] = value
+        # print("name: {name}, value: {value}".format(**locals()))
+        return value
+    return cached_value
+
+
+get_build_var.dict = {}
